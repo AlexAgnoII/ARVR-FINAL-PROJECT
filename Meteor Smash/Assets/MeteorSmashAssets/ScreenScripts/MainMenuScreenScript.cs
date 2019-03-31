5b1823f0f9de70a96a6b1a9ce7f1b6fa3a878aa2
Jess Gano <jesah_gano@dlsu.edu.ph>
2019-03-31 19:08:13 +0800

    fixed button thingy

--- Meteor Smash/Assets/MeteorSmashAssets/ScreenScripts/MainMenuScreenScript.cs
+++ Meteor Smash/Assets/MeteorSmashAssets/ScreenScripts/MainMenuScreenScript.cs
@@ -11,6 +11,7 @@ public class MainMenuScreenScript : View
 
     public void OnPlayClicked()
     {
+        Debug.Log("Play"); 
         ViewHandler.Instance.Show(ViewNames.MeteorSmash.LEVEL_SELECTION_SCREEN);
     }
 }
