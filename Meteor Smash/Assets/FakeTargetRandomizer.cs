cd8da4c3190a1871950cd3d35c757c34bec208b2
Jess Gano <jesah_gano@dlsu.edu.ph>
2019-04-08 14:02:22 +0800

    Update by Alex

--- Meteor Smash/Assets/FakeTargetRandomizer.cs
+++ Meteor Smash/Assets/FakeTargetRandomizer.cs
@@ -7,8 +7,8 @@ public class FakeTargetRandomizer : MonoBehaviour
 
     [SerializeField] private List<GameObject> targetList;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
         int real_target_index = Random.Range(0, 5);
         this.targetList[real_target_index].gameObject.tag = TagNames.TARGET;
